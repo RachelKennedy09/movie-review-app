209c593b6e7fe1fd6a9ac5729e201f40a4a92f86
RachelKennedy09 <rachelkennedy@live.ca>
2025-07-17 12:28:06 -0600

    Add basic Express server and MongoDB connection

---
+++ server.js
@@ -0,0 +1,41 @@
+// This file will Start my backend on port 3000
+//Connect to mongoDB
+//serve frontend (from public/)
+//prepare to handle routes for movies and reviews
+
+//Import dependencies
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+//configure environment variables
+dotenv.config();
+
+//create the express app
+const app = express();
+
+//Middleware to parse JSON
+app.use(express.json());
+
+//Server static files (HTML, CSS, JS) from public folder
+app.use(express.static("public"));
+
+//example default route
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "public" });
+});
+
+//connect to mongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Mongodb"))
+  .catch((err) => console.error("mongodb connection error:", err));
+
+//Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
